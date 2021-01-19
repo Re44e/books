beb168faee702161aec8ceeedba0a0fc2b94323e
Jean Pita <re44e@outlook.com.br>
2021-01-19 03:40:02 -0300

    Implementa novo teste de integração.

--- __tests__/integration/user.spec.ts
+++ __tests__/integration/user.spec.ts
@@ -9,17 +9,33 @@ import request from 'supertest'
 import { app } from '../../src/app'
 
 describe('User', () => {
-  it('Integration with user registration service', async () => {
+  it('Should integrate with the registration service', async () => {
 
     const response = await request(app)
     .post('/user')
     .send({
         name: 'smartmei',
-        email: 'test@smartmei.com.br',
+        email: 'smartmei@smartmei.com.br',
         
     });
 
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(201);
+  
+  });
+  
+  it('Should integrate with the book registration service', async () => {
+
+    const response = await request(app)
+    .post('/book')
+    .send({
+      logged_user_id: "6baff2bd-3ea0-4b3b-948f-3f1e059008bb",
+      title: "Rota 66",
+      pages: "200"
+      
+    });
+
+
+    expect(response.status).toBe(201);
   
   });
   
--- __tests__/units/operations.spec.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Área dedicada aos testes unitários:
- * Neste primeiro momento, daremos prioridade aos testes de integração.
- * Justamente para garantir, que os serviços essenciais da aplicação estão
- * operando conforme esperado. 
- * 
- * Metodologia de teste aplicada: "RGR" (Red Green Refactor)
- */
