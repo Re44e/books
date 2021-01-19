d8946b3e7fc1973e220007ece2928d301e117cf4
Jean Pita <re44e@outlook.com.br>
2021-01-18 21:30:15 -0300

    Corrige arquivos primários de teste.

--- __tests__/integration/user.spec.ts
+++ __tests__/integration/user.spec.ts
@@ -2,3 +2,23 @@
  * Área dedicada aos testes de integração:
  * Estes testes visam garantir que serviços fundamentais estão operando. 
  */
+
+import request from 'supertest'
+import { app } from '../../src/app'
+
+describe('User', () => {
+  it('Integration with user registration service', async () => {
+
+    const response = await request(app)
+    .post('/user')
+    .send({
+        name: 'smartmei',
+        email: 'test@smartmei.com.br',
+        
+    });
+
+    expect(response.status).toBe(200);
+  
+  });
+  
+});
--- __tests__/units/operations.spec.ts
+++ __tests__/units/operations.spec.ts
@@ -6,23 +6,3 @@
  * 
  * Metodologia de teste aplicada: "RGR" (Red - Green - Refactor)
  */
-
-import request from 'supertest'
-import { app } from '../../src/app'
-
-describe('User', () => {
-  it('Integration with user registration service', async () => {
-
-    const response = await request(app)
-    .post('/user')
-    .send({
-        name: 'smartmei',
-        email: 'test@smartmei.com.br',
-        
-    });
-
-    expect(response.status).toBe(200);
-  
-  });
-  
-});
